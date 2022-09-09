0bea316b9905a7a8397e10655e33d27fbeda6197
Willian Igor <yellowmoonastronaut@gmail.com>
2022-09-09 16:13:13 -0300

    add Calculadora de média

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,23 @@
 // DOM Loaded
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Coletando span
+    const result = document.querySelector("#result")
+    // Adicionando evento ao botão
+    document.querySelector("#tbnAverage").addEventListener('click', handlleAvrage)
+    
+    function handlleAvrage () {
+        // Coletando valores
+        const fristNote = parseFloat(document.querySelector("#firstNote").value)
+        const secondNote = parseFloat(document.querySelector("#secondNote").value)
+        const thirdNote = parseFloat(document.querySelector("#thirdNote").value)
+
+        const finalResult = ((fristNote + secondNote + thirdNote) / 3).toFixed(1)
+
+        if (finalResult > 0){
+            result.innerHTML = finalResult
+        } else {
+            result.innerHTML = "0"
+        }
+    }
 })
